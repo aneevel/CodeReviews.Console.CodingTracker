f415834815c3a44402e878c24e9786bc1c98ff6a
aneevel <aneevel15@gmail.com>
2025-12-22 16:26:28 -0600

    refactor appsettings to CodingTrackerEngine library

--- CodeReviews.Console.CodingTracker/Program.cs
+++ CodeReviews.Console.CodingTracker/Program.cs
@@ -1,15 +1,3 @@
-﻿using System.Text.Json;
-using CodeReviews.Console.CodingTracker;
-using CodingTrackerEngine;
-using Spectre.Console;
-
-string settingsFileName = "appsettings.json";
-string jsonSettingsString = File.ReadAllText(settingsFileName);
-
-AppSettings settings = JsonSerializer.Deserialize<AppSettings>(jsonSettingsString)!;
-
-AnsiConsole.MarkupLine(
-    $"[blue]Settings set based on configuration file;[/]\nDatabase Path: [green]{settings.Path}[/]\nDatabase Name: [green]{settings.Name}[/]\nConnection String: [green]{settings.ConnectionString}[/]"
-);
+﻿using CodingTrackerEngine;
 
 CodingTracker codingTracker = new CodingTracker();
