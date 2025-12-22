cd4635d225e2c3c8bde85dee2b3b6f762e9d9f63
aneevel <aneevel15@gmail.com>
2025-12-22 14:31:23 -0600

    add CodingSession model

---
+++ CodeReviews.Console.CodingTracker/Models/CodingSession.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeReviews.Console.CodingTracker.Models
+{
+    internal class CodingSession
+    {
+        private readonly int _id;
+        public int Id
+        {
+            get { return _id; }
+        }
+        private DateTime startTime { get; set; }
+        private DateTime endTime { get; set; }
+        private TimeSpan duration { get; set; }
+
+        public CodingSession(int id, DateTime startTime, DateTime endTime, TimeSpan duration)
+        {
+            _id = id;
+            this.startTime = startTime;
+            this.endTime = endTime;
+            this.duration = duration;
+        }
+    }
+}
