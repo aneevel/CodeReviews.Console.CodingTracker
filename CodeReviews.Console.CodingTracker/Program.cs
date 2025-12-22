870f250704062c371f9d616f15fe85e6984284ce
aneevel <aneevel15@gmail.com>
2025-12-22 14:52:29 -0600

    refactor session logic to dedicated library

--- CodeReviews.Console.CodingTracker/Program.cs
+++ CodeReviews.Console.CodingTracker/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System.Text.Json;
 using CodeReviews.Console.CodingTracker;
-using CodeReviews.Console.CodingTracker.Models;
+using CodingTrackerEngine;
 using Spectre.Console;
 
 string settingsFileName = "appsettings.json";
@@ -12,12 +12,4 @@ AnsiConsole.MarkupLine(
     $"[blue]Settings set based on configuration file;[/]\nDatabase Path: [green]{settings.Path}[/]\nDatabase Name: [green]{settings.Name}[/]\nConnection String: [green]{settings.ConnectionString}[/]"
 );
 
-CodingSession exampleSession = new CodingSession(
-    1,
-    new DateTime(2025, 12, 22, 12, 0, 0),
-    DateTime.Now
-);
-
-AnsiConsole.MarkupLine(
-    $"[blue]Example CodingSession contains parameters;[/]\nId: [green]{exampleSession.Id}[/]\nStart Time: [green]{exampleSession.StartTime}[/]\nEnd Time: [green]{exampleSession.EndTime}[/]\nDuration: [green]{exampleSession.Duration:c}[/]"
-);
+CodingTracker codingTracker = new CodingTracker();
