c4e1ad96fba7848e6575ffb4c3555df31bf4dfce
aneevel <aneevel15@gmail.com>
2025-12-22 14:27:19 -0600

    read in appsettings file

---
+++ CodeReviews.Console.CodingTracker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json;
+using CodeReviews.Console.CodingTracker;
+using Spectre.Console;
+
+string settingsFileName = "appsettings.json";
+string jsonSettingsString = File.ReadAllText(settingsFileName);
+
+AppSettings settings = JsonSerializer.Deserialize<AppSettings>(jsonSettingsString)!;
+
+AnsiConsole.MarkupLine(
+    $"[blue]Settings set based on configuration file;[/]\nDatabase Path: [green]{settings.Path}[/]\nDatabase Name: [green]{settings.Name}[/]\nConnection String: [green]{settings.ConnectionString}[/]"
+);
