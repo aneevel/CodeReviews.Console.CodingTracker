0590fefeb5ce8812913ddb7ecdfb65f2836d9ac4
aneevel <aneevel15@gmail.com>
2025-12-26 10:50:34 -0600

    remove old appsettings paradigm; use config manager

--- CodeReviews.Console.CodingTracker/CodingTracker.cs
+++ CodeReviews.Console.CodingTracker/CodingTracker.cs
@@ -21,18 +21,13 @@ namespace CodeReviews.Console.CodingTracker
 
         private readonly SqliteDatabaseManager _databaseManager;
 
-        public CodingTracker()
+        public CodingTracker(string connectionString)
         {
-            string settingsFileName = "appsettings.json";
-            string jsonSettingsString = File.ReadAllText(settingsFileName);
-
-            AppSettings settings = JsonSerializer.Deserialize<AppSettings>(jsonSettingsString)!;
-
             AnsiConsole.MarkupLine(
-                $"[blue]Settings set based on configuration file;[/]\nDatabase Path: [green]{settings.Path}[/]\nDatabase Name: [green]{settings.Name}[/]\nConnection String: [green]{settings.ConnectionString}[/]"
+                $"[blue]Settings set based on configuration file;[/]\nConnection String: [green]{connectionString}[/]"
             );
 
-            _databaseManager = new(settings.ConnectionString);
+            _databaseManager = new(connectionString);
 
             AnsiConsole.MarkupLine(
                 "Welcome to [purple]Coding Tracker![/] This amazing application allows you to record, modify, delete, and visualize your coding progress via individualized sessions!"
--- CodeReviews.Console.CodingTracker/Program.cs
+++ CodeReviews.Console.CodingTracker/Program.cs
@@ -1,3 +1,27 @@
 ﻿using CodeReviews.Console.CodingTracker;
+using Microsoft.Extensions.Configuration;
 
-CodingTracker codingTracker = new();
+try
+{
+    Init();
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"There was an error during application startup: {ex.Message}");
+}
+
+void Init()
+{
+    IConfiguration systemConfiguration = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json")
+        .Build();
+
+    var connectionString =
+        systemConfiguration.GetConnectionString("DefaultConnection")
+        ?? throw new InvalidOperationException(
+            "Unable to find default connection string; exiting..."
+        );
+
+    CodingTracker codingTracker = new(connectionString);
+}
