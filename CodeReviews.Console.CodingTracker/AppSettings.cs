5832a14357bba88dd76d5e4885fedc85af2ac91d
aneevel <aneevel15@gmail.com>
2025-12-26 10:33:31 -0600

    remove unneeded appSettings members

--- CodeReviews.Console.CodingTracker/AppSettings.cs
+++ CodeReviews.Console.CodingTracker/AppSettings.cs
@@ -1,17 +1,7 @@
 ﻿namespace CodeReviews.Console.CodingTracker
 {
-    internal class AppSettings(string path, string name, string connectionString)
+    internal class AppSettings(string connectionString)
     {
-        private readonly string _path = path;
-        public string Path
-        {
-            get { return _path; }
-        }
-        private readonly string _name = name;
-        public string Name
-        {
-            get { return _name; }
-        }
         private readonly string _connectionString = connectionString;
         public string ConnectionString
         {
