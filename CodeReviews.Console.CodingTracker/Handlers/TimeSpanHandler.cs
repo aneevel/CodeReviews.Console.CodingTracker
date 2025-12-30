8eb59f3e40560b06482b8e84e7e609fb98e4ad48
aneevel <aneevel15@gmail.com>
2025-12-30 13:08:09 -0600

    remove old package

--- CodeReviews.Console.CodingTracker/Handlers/TimeSpanHandler.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CodeReviews.Console.CodingTracker.Handlers
-{
-    public class TimeSpanHandler : SqliteTypeHandler<TimeSpan>
-    {
-        public override TimeSpan Parse(object value) => TimeSpan.Parse((string)value);
-    }
-}
